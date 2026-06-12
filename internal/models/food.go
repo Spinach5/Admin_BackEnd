75efd91f054720198bc05d250989b4f1368b54a2
欧立谢特 <16526078+WBK_111@user.noreply.gitee.com>
2026-06-12 17:32:28 +0800

    feat: 增加美食api

--- internal/models/food.go
+++ internal/models/food.go
@@ -48,6 +48,67 @@ func DeleteFood(db *sqlx.DB, id int) error {
 	return err
 }
 
+// GetFoodsBySchoolWithFilters 按学校ID和可选过滤条件查询食物
+func GetFoodsBySchoolWithFilters(db *sqlx.DB, schoolID, category, taste, canteenName, shopName, search string) ([]Food, error) {
+	query := "SELECT id, name, shop_name, canteen_name, school_id, price, taste, category FROM foods WHERE school_id = ?"
+	args := []interface{}{schoolID}
+
+	if category != "" {
+		query += " AND category = ?"
+		args = append(args, category)
+	}
+	if taste != "" {
+		query += " AND taste = ?"
+		args = append(args, taste)
+	}
+	if canteenName != "" {
+		query += " AND canteen_name = ?"
+		args = append(args, canteenName)
+	}
+	if shopName != "" {
+		query += " AND shop_name = ?"
+		args = append(args, shopName)
+	}
+	if search != "" {
+		query += " AND name LIKE ?"
+		args = append(args, "%"+search+"%")
+	}
+
+	query += " ORDER BY id"
+
+	foods := make([]Food, 0)
+	err := db.Select(&foods, query, args...)
+	return foods, err
+}
+
+// GetDistinctCategoriesBySchool 获取某学校下的所有食物分类
+func GetDistinctCategoriesBySchool(db *sqlx.DB, schoolID string) ([]string, error) {
+	var categories []string
+	err := db.Select(&categories, "SELECT DISTINCT category FROM foods WHERE school_id = ? AND category != '' ORDER BY category", schoolID)
+	return categories, err
+}
+
+// GetDistinctTastesBySchool 获取某学校下的所有食物口味
+func GetDistinctTastesBySchool(db *sqlx.DB, schoolID string) ([]string, error) {
+	var tastes []string
+	err := db.Select(&tastes, "SELECT DISTINCT taste FROM foods WHERE school_id = ? AND taste != '' ORDER BY taste", schoolID)
+	return tastes, err
+}
+
+// GetDistinctCanteensBySchool 获取某学校下的所有食堂名称
+func GetDistinctCanteensBySchool(db *sqlx.DB, schoolID string) ([]string, error) {
+	var canteens []string
+	err := db.Select(&canteens, "SELECT DISTINCT canteen_name FROM foods WHERE school_id = ? AND canteen_name != '' ORDER BY canteen_name", schoolID)
+	return canteens, err
+}
+
+// GetDistinctShopsBySchool 获取某学校下的所有店铺名称
+func GetDistinctShopsBySchool(db *sqlx.DB, schoolID string) ([]string, error) {
+	var shops []string
+	err := db.Select(&shops, "SELECT DISTINCT shop_name FROM foods WHERE school_id = ? AND shop_name != '' ORDER BY shop_name", schoolID)
+	return shops, err
+}
+
 func CreateFoodsBatch(db *sqlx.DB, foods []Food) (int, error) {
 	tx, err := db.Begin()
 	if err != nil {
